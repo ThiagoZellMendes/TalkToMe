1fa616cde404308dd86ec9aebb04cd7bf8009fef
Thiago Mendes <thiagosmendes.th@gmail.com>
2024-02-02 21:43:10 -0300

    Feat: Create-Initial-Page

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Container from './Container';
+
+export default function Header() {
+  return (
+    <div className='bg-gray-1000 p-4'>
+      <Container>
+        <div className='flex justify-between'>
+          <Image
+            alt='Talk to me'
+            src='/talktome.svg'
+            width={120}
+            height={120}
+          />
+          <Image alt='Hero Code' src='/logo.svg' width={60} height={60} />
+        </div>
+      </Container>
+    </div>
+  );
+}
