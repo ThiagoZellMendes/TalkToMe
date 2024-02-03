19496315b7421df73df33a08d90c802281f727d5
Thiago Mendes <thiagosmendes.th@gmail.com>
2024-02-02 21:47:51 -0300

    Chore: transform-svg-to-components

---
+++ src/Icons/index.ts
@@ -0,0 +1,10 @@
+export { default as CallLogo } from "./CallLogo";
+export { default as Desktop } from "./Desktop";
+export { default as Logo } from "./Logo";
+export { default as Mic } from "./Mic";
+export { default as NoDesktop } from "./NoDesktop";
+export { default as NoMic } from "./NoMic";
+export { default as NoVideo } from "./NoVideo";
+export { default as TalkToMe } from "./TalkToMe";
+export { default as Video } from "./Video";
+
