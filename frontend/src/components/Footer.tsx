ac43a9b5e84f6cbe288f341c5daa0e65e0e76d0d
Thiago Mendes <thiagosmendes.th@gmail.com>
2024-02-03 23:39:55 -0300

    Reafact: Implement-logic-on-Chat-And-Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,16 +8,98 @@ import {
   NoVideo,
   Video,
 } from '@/Icons';
-import { useState } from 'react';
+import { MutableRefObject, useState } from 'react';
 import Container from './Container';
 
-export default function Footer() {
+export default function Footer({
+  videoMediaStream,
+  peerConnections,
+  localStream,
+  logout,
+}: {
+  videoMediaStream: MediaStream;
+  peerConnections: MutableRefObject<Record<string, RTCPeerConnection>>;
+  localStream: MutableRefObject<HTMLVideoElement | null>;
+  logout: () => void;
+}) {
   const [isMuted, setIsMuted] = useState(false);
   const [isCameraOff, setIsCameraOff] = useState(false);
   const [isScreenSharing, setIsScreenSharing] = useState(false);
   const date = new Date();
   const hours = date.getHours().toString().padStart(2, '0') + ':';
   const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  const toggleMuted = () => {
+    videoMediaStream?.getAudioTracks().forEach((track) => {
+      track.enabled = !isMuted;
+    });
+    setIsMuted(!isMuted);
+
+    Object.values(peerConnections.current).forEach((peerConnection) => {
+      peerConnection.getSenders().forEach((sender) => {
+        if (sender.track?.kind === 'audio') {
+          if (videoMediaStream?.getAudioTracks().length > 0) {
+            sender.replaceTrack(
+              videoMediaStream
+                ?.getAudioTracks()
+                .find((track) => track.kind === 'audio') || null,
+            );
+          }
+        }
+      });
+    });
+  };
+
+  const toggleVideo = () => {
+    setIsCameraOff(!isCameraOff);
+    videoMediaStream?.getVideoTracks().forEach((track) => {
+      track.enabled = isCameraOff;
+    });
+
+    Object.values(peerConnections.current).forEach((peerConnection) => {
+      peerConnection.getSenders().forEach((sender) => {
+        if (sender.track?.kind === 'video') {
+          sender.replaceTrack(
+            videoMediaStream
+              ?.getVideoTracks()
+              .find((track) => track.kind === 'video') || null,
+          );
+        }
+      });
+    });
+  };
+
+  const toggleScreenSharing = async () => {
+    if (!isScreenSharing) {
+      const videoShareScreen = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
+      if (localStream.current) localStream.current.srcObject = videoShareScreen;
+
+      Object.values(peerConnections.current).forEach((peerConnections) => {
+        peerConnections.getSenders().forEach((sender) => {
+          if (sender.track?.kind === 'video') {
+            sender.replaceTrack(videoShareScreen.getVideoTracks()[0]);
+          }
+        });
+      });
+
+      setIsScreenSharing(!isScreenSharing);
+      return;
+    }
+
+    if (localStream.current) localStream.current.srcObject = videoMediaStream;
+
+    Object.values(peerConnections.current).forEach((peerConnections) => {
+      peerConnections.getSenders().forEach((sender) => {
+        if (sender.track?.kind === 'video') {
+          sender.replaceTrack(videoMediaStream?.getVideoTracks()[0]);
+        }
+      });
+    });
+    setIsScreenSharing(!isScreenSharing);
+  };
+
   return (
     <div className='fixed items-center bottom-0 bg-black py-6 w-full'>
       <Container>
@@ -32,39 +114,42 @@ export default function Footer() {
             {isMuted ? (
               <NoMic
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-red-500  rounded-md'
-                onClick={() => setIsMuted(!isMuted)}
+                onClick={() => toggleMuted()}
               />
             ) : (
               <Mic
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-gray-950  rounded-md'
-                onClick={() => setIsMuted(!isMuted)}
+                onClick={() => toggleMuted()}
               />
             )}
             {isCameraOff ? (
-              <NoVideo
+              <Video
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-red-500 rounded-md'
-                onClick={() => setIsCameraOff(!isCameraOff)}
+                onClick={() => toggleVideo()}
               />
             ) : (
-              <Video
+              <NoVideo
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-gray-950 rounded-md'
-                onClick={() => setIsCameraOff(!isCameraOff)}
+                onClick={() => toggleVideo()}
               />
             )}
 
             {isScreenSharing ? (
               <NoDesktop
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-red-500 rounded-md'
-                onClick={() => setIsScreenSharing(!isScreenSharing)}
+                onClick={() => toggleScreenSharing()}
               />
             ) : (
               <Desktop
                 className='h-12 w-16 text-white p-2 cursor-pointer bg-gray-950 rounded-md'
-                onClick={() => setIsScreenSharing(!isScreenSharing)}
+                onClick={() => toggleScreenSharing()}
               />
             )}
 
-            <CallLogo className='h-12 w-16 text-white hover:bg-red-500 p-2 cursor-pointer bg-primary rounded-md' />
+            <CallLogo
+              onClick={logout}
+              className='h-12 w-16 text-white hover:bg-red-500 p-2 cursor-pointer bg-primary rounded-md'
+            />
           </div>
         </div>
       </Container>
